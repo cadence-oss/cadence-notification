04fac2fee88436bb30e0c9c253df1f5f9f5a86a5
Quanzheng Long <qlong@indeed.com>
2021-11-03 21:43:58 -0700

    header

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 Cadence OSS
+// Copyright (c) 2021 Cadence workflow OSS organization
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
