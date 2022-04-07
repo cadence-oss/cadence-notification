f6dadae4d463fac573c5c37f8c844148e0d2c76c
Quanzheng Long <qlong@indeed.com>
2022-04-06 21:32:54 -0700

    Done

--- start.sh
+++ start.sh
@@ -6,5 +6,9 @@ CONFIG_TEMPLATE_PATH="${CONFIG_TEMPLATE_PATH:-/etc/cadence-notification/config/c
 
 dockerize -template $CONFIG_TEMPLATE_PATH:/etc/cadence-notification/config/docker.yaml
 
+# TODO https://github.com/cadence-oss/cadence-notification/issues/23
+# depends_on in docker-compose doesn't work and the startup will fail at waiting for Kafka to be up.
+# waiting for 5 second here can mitigate it.
+sleep 5
 exec cadence-notification --root $CADENCE_NOTIFICATION_HOME --env docker start --services=$SERVICES
 
