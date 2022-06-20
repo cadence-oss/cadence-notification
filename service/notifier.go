96476d81095a3fb19a2210f02fbbea52943c0925
duoertai <duoertai@gmail.com>
2022-06-19 18:55:59 -0700

    use cadence master branch as dep, refactor code accordingly

--- service/notifier.go
+++ service/notifier.go
@@ -195,7 +195,7 @@ func (p *notifier) notifySubscriber(decodedMsg *indexer.Message, kafkaMsg messag
 			_ = kafkaMsg.Nack()
 		}
 
-		backoff.Retry(
+		Retry(
 			func() error { return p.sendMessageToWebhook(notification, webhook) },
 			p.retryPolicy,
 			nil)
@@ -317,4 +317,38 @@ func (p *notifier) sendMessageToWebhook(notification *Notification, webhook *con
 	body, _ := ioutil.ReadAll(resp.Body)
 	p.logger.Debug(fmt.Sprintf("response Body: %v", string(body)))
 	return nil
+}
+
+func Retry(operation backoff.Operation, policy backoff.RetryPolicy, isRetryable backoff.IsRetryable) error {
+	var prevErr error
+	var err error
+	var next time.Duration
+
+	r := backoff.NewRetrier(policy, backoff.SystemClock)
+	for {
+		// record the previous error before an operation
+		prevErr = err
+
+		// operation completed successfully.  No need to retry.
+		if err = operation(); err == nil {
+			return nil
+		}
+
+		if next = r.NextBackOff(); next == -1 {
+			if prevErr != nil {
+				return prevErr
+			}
+			return err
+		}
+
+		// Check if the error is retryable
+		if isRetryable != nil && !isRetryable(err) {
+			if prevErr != nil {
+				return prevErr
+			}
+			return err
+		}
+
+		time.Sleep(next)
+	}
 }
